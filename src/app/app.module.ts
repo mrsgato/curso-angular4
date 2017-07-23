115b05a4a517b5c76ee5a5cbdc1745a0c8f40302
gatocu <bernardo.machuca@gmail.com>
2017-07-23 02:13:31 -0400

    avance sabado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +14,8 @@ import { EmpleadoComponent } from './empleado/empleado.component';
     EmpleadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -9,8 +9,36 @@ import {Empleado} from './empleado';
 export class EmpleadoComponent{
     public titulo = "Componente empleado";
     public empleado:Empleado;
-    ngOnInit(){
+    public trabajadores:Array<Empleado>;
+    public trabajador_externo:boolean;
+    public color:string;
+    public color_seleccionado:string;
+
+    constructor(){
         this.empleado = new Empleado("yoni",45,"recolector",true);
+        this.trabajadores = [
+            new Empleado('manolo manolete', 18,"corredor",false),
+            new Empleado('machete', 25,"transportador",true),
+            new Empleado('waripolo', 38,"vendedor",true)
+        ]
+
+        this.trabajador_externo = true;
+        this.color = 'green';
+        this.color_seleccionado = '#ccc';
+    }
+
+    
+
+    ngOnInit(){
         console.log(this.empleado);
+        console.log(this.trabajadores);
+    }
+
+    cambiarExterno(valor){
+        this.trabajador_externo = valor;
+    }
+
+    logColorSeleccionado(){
+        console.log(this.color_seleccionado);
     }
 }
